a0264a4e751110ff171179c705a8fe84e8badee1
akimovaarai <63499887+akimovaarai@users.noreply.github.com>
2020-04-11 19:48:57 +0600

    Add files via upload

---
+++ IBMCapstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Jupyter notebook for IBM Capstone
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
